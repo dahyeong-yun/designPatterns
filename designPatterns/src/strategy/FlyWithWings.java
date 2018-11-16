493c179096c0518016811da6d58ce92495a76b4a
danny <gryb809@gmail.com>
2018-11-16 18:26:58 +0900

    Upload strategy pattern source

---
+++ designPatterns/src/strategy/FlyWithWings.java
@@ -0,0 +1,12 @@
+package strategy;
+
+public class FlyWithWings implements FlyBehavior {
+
+	// 날아다니는 것을 구현
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ designPatterns/src/strategy/Quack.java
@@ -0,0 +1,12 @@
+package strategy;
+
+public class Quack implements QuackBehavior {
+
+	// 꽥꽥 소리를 냄
+	@Override
+	public void quack() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
